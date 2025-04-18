5db8a21f38495bd713351b490526136cdb476be4
sivakinfomagnus <siva.kakani@infomagnus.com>
2025-04-18 14:31:46 -0400

    test

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,14 @@
-provider "azurerm" {
-  features {}
+# provider "azurerm" {
+#   features {}
+# }
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "azure-play-terraform-state-rg"
+    storage_account_name = "tfstate25191"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
--- state_storage.sh
+++ state_storage.sh
@@ -1,16 +1,26 @@
 # Set variables
-RESOURCE_GROUP="azure-play-rg"
-STORAGE_ACCOUNT="tfstateazureplay"  # must be globally unique
+RG_NAME="azure-play-terraform-state-rg"
+STORAGE_ACCOUNT="tfstate$RANDOM"   # must be globally unique
 CONTAINER_NAME="tfstate"
 
 # Create resource group
-az group create --name $RESOURCE_GROUP --location eastus
+az group create --name $RG_NAME --location eastus
 
 # Create storage account
-az storage account create --name $STORAGE_ACCOUNT --resource-group $RESOURCE_GROUP --sku Standard_LRS --encryption-services blob
+az storage account create \
+  --name $STORAGE_ACCOUNT \
+  --resource-group $RG_NAME \
+  --sku Standard_LRS \
+  --encryption-services blob
 
-# Get the storage key
-ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP --account-name $STORAGE_ACCOUNT --query '[0].value' -o tsv)
+# Get access key
+ACCOUNT_KEY=$(az storage account keys list \
+  --resource-group $RG_NAME \
+  --account-name $STORAGE_ACCOUNT \
+  --query '[0].value' -o tsv)
 
-# Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT --account-key $ACCOUNT_KEY
+# Create container
+az storage container create \
+  --name $CONTAINER_NAME \
+  --account-name $STORAGE_ACCOUNT \
+  --account-key $ACCOUNT_KEY
